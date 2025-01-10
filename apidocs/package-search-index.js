77116d721bc0512cafca778e4a1fb1c5fa85c958
Laird Nelson <laird.nelson@oracle.com>
2025-01-10 14:34:58 -0800

    Site checkin for project microBean™ Event

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"org.microbean.event","l":"org.microbean.event"}];updateSearchResults();
---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.microbean.event","l":"EventQualifiersMatcher"},{"p":"org.microbean.event","l":"EventTypeMatcher"},{"p":"org.microbean.event","l":"EventTypes"}];updateSearchResults();
